591544a9390204fc8560f0ff474e29f2e1c3054b
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-04-16 21:36:32 +0530

    assignment 5 programes

--- random_add_2dice.sh
+++
@@ -1,6 +0,0 @@
-dice1=$((RANDOM%6))
-echo $dice1
-dice2=$((RANDOM%6))
-echo $dice2
-sum=$((dice1+dice2))
-echo $sum
--- unit_conversion1.sh
+++
@@ -1,5 +0,0 @@
-read -p "enter number n= " n
-feet=$(($n*12))
-inches=$(($n/12))
-echo "$feet"
-echo "$inches"
